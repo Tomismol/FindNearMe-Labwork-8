97570c6e3f0673dd51b3ba063a89a58d79a19492
Tomismol <tom.smolarek01@gmail.com>
2018-11-27 21:05:03 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-//import { TabsPage } from '../pages/tabs/tabs';
-//import { WelcomePage } from '../pages/welcome/Welcome';
-
-import { Login } from '../pages/login/login';
+import { WelcomePage } from '../pages/welcome/Welcome';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = Login;
+  rootPage:any = WelcomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
